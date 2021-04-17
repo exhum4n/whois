729104935c3fa4fe44e14d941768ec15902b03e8
Станислав Чурилов <exhum4n.dev@gmail.com>
2021-04-17 11:49:45 +0300

    prepare to done

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default user location
+    |--------------------------------------------------------------------------
+    |
+    | This data will be used by defaults if can not receive location
+    | data from service.
+    |
+    */
+
+    'location' => [
+        'default' => [
+            'ru' => 'ru-RU',
+            'en' => 'en-US',
+        ]
+    ]
+];
